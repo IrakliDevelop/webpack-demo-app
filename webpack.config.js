e39bd8733a464963029e0bdc2058b5b68d9e7f04
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-08-26 21:51:05 +0400

    from css to scss

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,12 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
+                test: /\.scss$/,
+                use: [
+                    'style-loader', //3rd. Inject styles into DOM
+                    'css-loader', //2nd. Turns css into commonjs
+                    'sass-loader', // 1st. Turns sass into css
+                ]
             }
         ]
     }
