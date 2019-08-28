48f79d99bd85cb4f084fa1274d1db285baaa67f6
IrakliDevelop <i_iremashvili@cu.edu.ge>
2019-08-28 21:13:28 +0400

    install and configure mini-css-extract-plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const common = require('./webpack.common');
 
@@ -10,5 +11,8 @@ module.exports = merge(common, {
         filename: '[name].[contentHash].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [new CleanWebpackPlugin()], // used to delete old files and recreate dist folder on build
+    plugins: [
+        new CleanWebpackPlugin(), // used to delete old files and recreate dist folder on build
+        new MiniCssExtractPlugin({ filename: '[name].[contentHash].css'})
+    ], 
 });
